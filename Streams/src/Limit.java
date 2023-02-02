f7a0198342d9e46f38a3da903999f6bb2bb09b0f
nithin <nithin.chandra.patnam@ey.com>
2023-02-02 12:20:25 -0600

    distinct, limit,peek

---
+++ Streams/src/Limit.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Limit {
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("one");
+        stringList.add("two");
+        stringList.add("three");
+        stringList.add("one");
+        Stream<String> stream = stringList.stream();
+        stream.limit(2)
+                .forEach( element -> { System.out.println(element); });
+    }
+}
+
